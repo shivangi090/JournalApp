9f97e1e71c1425d2a9e48865e7f94705de139ee6
SHIVANGI\Shivangi <shivangiarorambd@gmail.com>
2025-10-05 18:00:27 +0530

    Role Based Authentication

--- journalApp/src/main/java/net/engineeringdigest/journalApp/entity/User.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/entity/User.java
@@ -11,7 +11,6 @@ import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
